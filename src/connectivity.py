2a6a743197348a23aba691ee799435d972ea970e
Anogio <antoine.ogier@gmail.com>
2024-02-15 13:50:55 +0100

    Broad websocket catch

--- src/connectivity.py
+++ src/connectivity.py
@@ -17,13 +17,12 @@ class WebsocketConnectionPool:
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    @staticmethod
-    async def send_personal_message(message: dict, websocket: WebSocket):
-        await websocket.send_json(message)
+    async def send_personal_message(self, message: dict, websocket: WebSocket):
+        try:
+            await websocket.send_json(message)
+        except Exception:
+            self.disconnect(websocket)
 
     async def broadcast(self, message: dict):
         for connection in self.active_connections:
-            try:
-                await connection.send_json(message)
-            except Exception:
-                self.disconnect(connection)
+            await self.send_personal_message(message, connection)
